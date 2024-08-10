834205468a964de7a9f34912e64c5b3e1f26d8c5
hoangdesu <hoangdesu@gmail.com>
2024-08-10 12:04:27 +0700

    change content on agent click

--- web.js
+++ web.js
@@ -1,3 +1,47 @@
 // const getIcon = async () => {
 
 // }
+
+// var glide = new Glide(".glide", {
+//     type: "carousel",
+//     focusAt: "center",
+//     perView: 3
+//   });
+
+// glide.mount();
+  
+
+const agentsGrid = document.querySelector("#agents-grid");
+const agentPortrait = document.querySelector('#agent-portrait');
+const agentBg = document.querySelector('#agent-bg');
+
+
+const API_URL = "https://valorant-api.com/v1/agents";
+fetch(API_URL)
+  .then((responseObject) => responseObject.json())
+  .then((resJson) => {
+    // console.log(resJson);
+    const { data: agentList } = resJson; // deconstruct resJson object && rename data to agentList
+    // console.log(data); // resJson.data
+    console.log(agentList);
+
+    agentList.forEach((agent) => {
+      const { fullPortraitV2, background } = agent; // no need agent.fullPortraitV2
+
+      const li = document.createElement('li');
+
+      li.addEventListener('click', () => {
+        agentPortrait.src = fullPortraitV2;
+        agentBg.src = background;
+      })
+      
+      const agentImage = `<img src="${fullPortraitV2}" alt="" width="400"></img>`;
+      li.innerHTML += agentImage;
+
+    //   agentsGrid.innerHTML += li;
+        agentsGrid.appendChild(li);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
